aab3c066b4501c35ff3f0eefae9ffc12bde86014
leozhou <leo@leozhou.me>
2020-09-26 17:13:16 +0800

    Initial Commit

---
+++ todo-Kavsoft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-Kavsoft
+//
+//  Created by 周椿杰 on 2020/9/26.
+//  Copyright © 2020 周椿杰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
